522e6f14f7ccefddb25bbcf0b1da6af21d266008
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-23 16:16:19 +0800

    Remove the domme data in Post model

--- CafafansCodersService/posts/views.py
+++ CafafansCodersService/posts/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import User
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'id': 1, 
-        'author': 'Emeka Augustine', 
-        'title': 'First Bog Post',
-        'content': 'The first post is so beautiful!',
-        'date_posted': 'August 14th, 2019'
-    },
-    {
-        'id': 2, 
-        'author': 'Emeka Augustine', 
-        'title': 'Second Bog Post',
-        'content': 'The second blog post is so beautiful!',
-        'date_posted': 'August 24th, 2019'
-    }
-]
-
 def index(request):
-    posts_data = {
-        'posts': posts,
+    posts = {
+        'posts': Post.objects.all(),
         'title': 'Main Posts'
     }
-    return render(request, 'posts/index.html', posts_data)
+    return render(request, 'posts/index.html', posts)
