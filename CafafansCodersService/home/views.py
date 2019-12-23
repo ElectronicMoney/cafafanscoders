4db676dd87d4a3d73747d93520a4c5c5a1abaf77
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-23 10:48:47 +0800

    Create Home Page

---
+++ CafafansCodersService/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# from .models import User
+
+# Create your views here.
+# def index(request):
+#     users = {
+#         'users': User.objects.all()
+#     }
+#     return render(request, 'home.html', users)
+
+def index(request):
+    return HttpResponse('<h1>Welcome Cafafans Coders!</h1>')
