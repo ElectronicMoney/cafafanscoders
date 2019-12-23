8139f8d1fc663a04e2f4fa22e9d7e6880322c9e1
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-23 12:57:51 +0800

    Add layouts templates

--- CafafansCodersService/posts/views.py
+++ CafafansCodersService/posts/views.py
@@ -9,5 +9,25 @@ from django.http import HttpResponse
 #     }
 #     return render(request, 'home.html', users)
 
+posts = [
+    {
+        'id': 1, 
+        'author': 'Emeka Augustine', 
+        'title': 'First Bog Post',
+        'content': 'The first post is so beautiful!',
+        'date_posted': 'August 14th, 2019'
+    },
+    {
+        'id': 2, 
+        'author': 'Emeka Augustine', 
+        'title': 'Second Bog Post',
+        'content': 'The second blog post is so beautiful!',
+        'date_posted': 'August 24th, 2019'
+    }
+]
+
 def index(request):
-    return render(request, 'posts/index.html')
+    posts_data = {
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', posts_data)
