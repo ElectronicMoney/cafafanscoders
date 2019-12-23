ede8363326a209076348d444724d2642a34c74b4
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-23 17:09:46 +0800

    Add postgresql database

--- CafafansCodersService/CafafansCodersService/settings.py
+++ CafafansCodersService/CafafansCodersService/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'CafafansCodersService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cafafanscoders',
+        'USER': 'cafafanscoders',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
