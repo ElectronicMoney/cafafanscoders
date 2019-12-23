380dd0aca209ec365c636857e4d0499f463fb274
Emeka Augustine <cafafanscoders@gmail.com>
2019-12-23 12:43:46 +0800

    Fix minor bugs

--- CafafansCodersService/posts/views.py
+++ CafafansCodersService/posts/views.py
@@ -10,4 +10,4 @@ from django.http import HttpResponse
 #     return render(request, 'home.html', users)
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'posts/index.html')
